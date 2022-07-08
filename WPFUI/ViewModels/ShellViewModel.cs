09c645b89512b22a479a80d295076da8bde71848
Felix Schneidenbach <croekk@gmail.com>
2022-07-08 20:29:31 +0200

    Added WPFUI

---
+++ WPFUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Caliburn.Micro;
+using ConsoleProject;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        #region Private Fields
+        private BindableCollection<DeckModel> _decks = new();
+        private DeckModel _selectedDeck;
+        #endregion
+        public ShellViewModel()
+		{
+			SqliteCrud sql = new SqliteCrud(GlobalConfig.GetConnectionString());
+			_decks = new BindableCollection<DeckModel>(sql.ReadAllDecks());
+		}
+
+		public bool CreateFlashcardButtonIsVisible { get; set; }
+		public DeckModel SelectedDeck
+		{
+			get { return _selectedDeck; }
+			set 
+			{ 
+				_selectedDeck = value;
+                NotifyOfPropertyChange(() => SelectedDeck);
+				CreateFlashcardButtonIsVisible = true;
+            }
+		}						
+
+		public BindableCollection<DeckModel> Decks
+		{
+			get { return _decks; }
+			set { _decks = value; }
+		}
+
+
+	}
+}
